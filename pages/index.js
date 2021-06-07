0f1df10f2efa5d7c2b3ecc2a18519e151dc3098a
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2021-06-07 16:56:00 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-semibold text-gray-800 mt-10">
+        <a
+          href="https://nextjs.org"
+          target="_blank"
+          className="text-blue-600 hover:underline"
+        >
+          Next.js
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://tailwindcss.com"
+          target="_blank"
+          className="text-blue-600 hover:underline"
+        >
+          Tailwind CSS
+        </a>{" "}
+        Starter
+      </h1>
+    </div>
+  );
+}
